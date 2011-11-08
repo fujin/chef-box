1a02765ccabbc59ca32fe8a1dac21581ee6244ab
AJ Christensen <aj@junglist.gen.nz>
2011-11-09 12:34:37 +1300

    light weight chef install

--- definitions/.common/chef.sh
+++ definitions/.common/chef.sh
@@ -1,45 +1,13 @@
 #!/bin/bash -ex
 
-# Java
-perl -p -i -e 's/^\# (.*partner)$/$1/g' /etc/apt/sources.list
 apt-get update
-echo "
-sun-java6-bin shared/accepted-sun-dlj-v1-1 boolean true
-sun-java6-bin shared/present-sun-dlj-v1-1 note
-sun-java6-jdk shared/accepted-sun-dlj-v1-1 boolean true
-sun-java6-jdk shared/present-sun-dlj-v1-1 note
-sun-java6-jre shared/accepted-sun-dlj-v1-1 boolean true
-sun-java6-jre shared/present-sun-dlj-v1-1 note
-sun-java6-jre sun-java6-jre/jcepolicy note
-sun-java6-jre sun-java6-jre/stopthread boolean true
-" | debconf-set-selections
-apt-get -y install sun-java6-jdk
+apt-get install -y ruby ruby1.8-dev build-essential wget libruby-extras libruby1.8-extras
+cd /tmp
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.6.2.tgz
+tar zxf rubygems-1.6.2.tgz
+cd rubygems-1.6.2
+ruby setup.rb --no-format-executable
+gem update --no-rdoc --no-ri
+gem install ohai --no-rdoc --no-ri --verbose
+gem install chef --no-rdoc --no-ri --verbose
 
-# RabbitMQ
-echo "rabbitmq-server rabbitmq-server/upgrade_previous note" |\
-    debconf-set-selections
-
-# Chef
-echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main" |\
-    tee /etc/apt/sources.list.d/opscode.list
-wget -O - http://apt.opscode.com/packages@opscode.com.gpg.key |\
-    apt-key add -
-apt-get update
-echo "
-chef chef/chef_server_url string http://localhost:4000
-chef-server-webui chef-server-webui/admin_password string ub3rs3kr4t
-chef-solr chef-solr/amqp_password string ub3rs3kr4t
-" | debconf-set-selections
-apt-get -y install chef-server
-
-# Cleanup
-for x in chef-client chef-server-webui chef-server chef-expander chef-solr \
-    rabbitmq-server couchdb ; do
-    update-rc.d -f $x remove &
-    invoke-rc.d $x stop &
-    wait
-done
-chown -R chef:chef /etc/chef /var/lib/chef /var/log/chef
-chown -R rabbitmq:rabbitmq /etc/rabbitmq /var/lib/rabbitmq /var/log/rabbitmq
-chown -R couchdb:couchdb /etc/couchdb /var/lib/couchdb /var/log/couchdb
-rm -rf /etc/chef/client.pem /var/lib/rabbitmq/* /var/lib/couchdb/*
