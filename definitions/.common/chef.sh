7ce01f9eb95a4670ccd3a7b12585d5733db8366f
AJ Christensen <aj@junglist.gen.nz>
2012-04-03 11:03:18 +1200

    oneiric64

--- definitions/.common/chef.sh
+++ definitions/.common/chef.sh
@@ -1,24 +1,20 @@
 #!/bin/bash -ex
 
 exists() {
-  if command -v $1 &>/dev/null
-  then
-    return 0
-  else
-    return 1
-  fi
+    if command -v $1 &>/dev/null
+    then
+        return 0
+    else
+        return 1
+    fi
 }
 
 install_sh="http://opscode.com/chef/install.sh"
 
 if ! exists /usr/bin/chef-client; then
-  if exists wget; then
-    bash <(wget ${install_sh} -O -)
-  else
     if exists curl; then
-      bash <(curl -L ${install_sh})
+        curl -L ${install_sh} | bash
     fi
-  fi
 fi
 
 mkdir -p /etc/chef
