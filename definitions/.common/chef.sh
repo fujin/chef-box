72cc46801e3c4f255f56fcecc753e0b9678dc33e
AJ Christensen <aj@junglist.gen.nz>
2012-04-03 15:17:36 +1200

    reduce complexity. dash is not fully functional "sh"

--- definitions/.common/chef.sh
+++ definitions/.common/chef.sh
@@ -3,21 +3,8 @@
 apt-get update
 apt-get install wget -y
 
-exists() {
-    if command -v $1 &>/dev/null
-    then
-        return 0
-    else
-        return 1
-    fi
-}
-
 install_sh="http://opscode.com/chef/install.sh"
 
-if ! exists /usr/bin/chef-client; then
-    if exists wget; then
-        wget ${install_sh} -O - | bash
-    fi
-fi
+wget ${install_sh} && bash install.sh
 
 mkdir -p /etc/chef
